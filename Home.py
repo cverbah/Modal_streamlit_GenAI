9f858b90cd91805c7b52349086fc5875e651f5c2
cvergarabah <c_vergara@hotmail.com>
2024-07-06 16:43:59 -0400

    update file upload

--- Home.py
+++ Home.py
@@ -28,38 +28,41 @@ try:
         #Dataframe formatting
         if 'df' not in st.session_state:
             if uploaded_file.name.endswith('.csv'):
-                df = pd.read_csv(temp_location, index_col=0)
+                df = pd.read_csv(uploaded_file, index_col=0)
+                df = df.convert_dtypes()
+                df.columns = df.columns.str.lower()
                 #df = format_compete_table(df)   # deactivated for now
 
                 # por ahora este preprocessing para una tabla de prueba que estoy usando (ofertas_test.csv)
                 # parse data
-                df['marcas_en_promo'] = df['marcas_en_promo'].apply(lambda row: parse_null_list(row))
-                df['marcas_en_promo'] = df['marcas_en_promo'].apply(literal_eval)
-                df['publico_objetivo'] = df['publico_objetivo'].apply(lambda row: parse_null_list(row))
-                df['publico_objetivo'] = df['publico_objetivo'].apply(literal_eval)
-                df['categorias_en_promo'] = df['categorias_en_promo'].apply(lambda row: parse_null_list(row))
-                df['categorias_en_promo'] = df['categorias_en_promo'].apply(literal_eval)
-                df['productos_en_oferta'] = df['productos_en_oferta'].apply(lambda row: parse_null_list(row))
-                df['datetime_checked'] = pd.to_datetime(df['datetime_checked'])
-                df['date_checked'] = df['datetime_checked'].dt.date
-                df['descuentos_promo'] = df['descuentos_promo'].str.rstrip('%')
-                df['descuentos_promo'] = pd.to_numeric(df['descuentos_promo'], errors='coerce')
+                #df['marcas_en_promo'] = df['marcas_en_promo'].apply(lambda row: parse_null_list(row))
+                #df['marcas_en_promo'] = df['marcas_en_promo'].apply(literal_eval)
+                #df['publico_objetivo'] = df['publico_objetivo'].apply(lambda row: parse_null_list(row))
+                #df['publico_objetivo'] = df['publico_objetivo'].apply(literal_eval)
+                #df['categorias_en_promo'] = df['categorias_en_promo'].apply(lambda row: parse_null_list(row))
+                #df['categorias_en_promo'] = df['categorias_en_promo'].apply(literal_eval)
+                #df['productos_en_oferta'] = df['productos_en_oferta'].apply(lambda row: parse_null_list(row))
+                #df['datetime_checked'] = pd.to_datetime(df['datetime_checked'])
+                #df['date_checked'] = df['datetime_checked'].dt.date
+                #df['descuentos_promo'] = df['descuentos_promo'].str.rstrip('%')
+                #df['descuentos_promo'] = pd.to_numeric(df['descuentos_promo'], errors='coerce')
                 # Divide by 100 to convert to decimal form
-                df['descuentos_promo'] = df['descuentos_promo'] / 100
+                #df['descuentos_promo'] = df['descuentos_promo'] / 100
                 # Save the data to session state
                 st.session_state.df = df
 
             if uploaded_file.name.endswith(('.xls', '.xlsx')):
                 df = pd.read_excel(uploaded_file, engine='openpyxl')
+                df = df.convert_dtypes()
+                df.columns = df.columns.str.lower()
                 # por ahora este preprocessing para una tabla de prueba que estoy usando (Purina Specialty - Catálogo de sustitutos.xlsx)
                 # columnas con precios
-                precios_tienda = df.iloc[:, 4:]
-                for col in precios_tienda.columns:
-                    df[col] = pd.to_numeric(df[col], errors='coerce')
-
+                #precios_tienda = df.iloc[:, 4:]
+                #for col in precios_tienda.columns:
+                #    df[col] = pd.to_numeric(df[col], errors='coerce')
                 #df = df.fillna(0)
-                df = df[~df.SKU.isnull()]
-                df.columns = df.columns.str.lower()
+                #df = df[~df.SKU.isnull()]
+
                 # Save the data to session state
                 st.session_state.df = df
 
