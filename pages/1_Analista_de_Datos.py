fd7566857b7061e36615173294116c3536f1062f
cvergarabah <c_vergara@hotmail.com>
2024-10-21 11:10:35 -0300

    udpate LLM for AI Assistant. Add memory

--- pages/1_Analista_de_Datos.py
+++ pages/1_Analista_de_Datos.py
@@ -8,20 +8,26 @@ import seaborn as sns
 import plotly.graph_objects as go
 matplotlib.use('Agg')
 
+st.set_page_config(
+    page_title="AI Data Analyst",
+    page_icon=":robot_face:",
+    layout="wide",
+)
+
 try:
     df = st.session_state.df
 
     if 'df_filtered' in st.session_state:
         if len(df) > 0:
-            st.dataframe(st.session_state.df[:5000])
+            st.dataframe(st.session_state.df[:5000], height=200)
             aux = 1
 
         else:
-            st.dataframe(st.session_state.df[:5000])
+            #st.dataframe(st.session_state.df[:5000])
             st.write('Revise sus filtros: No hay datos para los filtros seleccionados')
 
     else:
-        st.dataframe(st.session_state.df[:5000])
+        st.dataframe(st.session_state.df[:5000], height=200)
         aux = 0
 
     tab1, tab2, tab3, tab4 = st.tabs(["Analista de datos AI", "Tabla dinámica", "Insights", "dummy"])
@@ -29,31 +35,48 @@ try:
         if 'user_input' not in st.session_state:
             st.session_state.user_input = ''
 
-        st.text_input('Qué desea saber de la tabla?', key='widget', on_change=submit_query)
-        st.write(f'Última consulta: {st.session_state.user_input}')
+        col1, col2 = st.columns([0.8, 0.12], gap='large')
+        with col1:
+            st.text_input('', key='widget', on_change=submit_query,
+                          placeholder='Qué desea saber de la tabla?')
+
+            if st.session_state.user_input != '':
+                st.write(f'Última consulta: {st.session_state.user_input}')
+
+        with col2:
+            st.write('')
+            st.button('Reset', on_click=reset_memory)
 
         # Display the stored user input
         if st.session_state.user_input != '':
 
+            st.session_state.user_input = fix_query(st.session_state.user_input)
             col1, col2 = st.columns(2, gap='large')
             with col1:
                 with st.spinner('Pensando...'):
                     try:
-                        if 'df_filtered' in st.session_state:
-                            response = analyze_table_gemini(query=st.session_state.user_input, df=st.session_state.df,
-                                                            plot_type='plotly')
+                        if 'ai_assistant' not in st.session_state:
+                            st.session_state.ai_assistant = 0
 
-                            local_vars, output = execute_code(response, df=st.session_state.df)
-                        else:
-                            response = analyze_table_gemini(query=st.session_state.user_input, df=df,
-                                                            plot_type='plotly')
-                            local_vars, output = execute_code(response, df=df)
+                        if st.session_state.ai_assistant == 0:
+                            st.session_state.chain = generate_llm_chain(df=df)
+                            st.session_state.chat_memory = ChatMessageHistory()
+                            st.session_state.ai_memory = add_memory_chain(st.session_state.chain,
+                                                                          st.session_state.chat_memory)
+                            st.session_state.ai_assistant = 1
+
+                        if st.session_state.ai_assistant:
+                            response = st.session_state.ai_memory.invoke(
+                                {"input": st.session_state.user_input},
+                                {"configurable": {"session_id": "unused"}},
+                            )
+                        local_vars, output = execute_code(response, df=df)
 
                         st.write('Python Snippet:')
                         st.text(response)
 
                     except Exception as e:
-                        st.text('Error al ejecutar la query. Intente de nuevo modificando su consulta.')
+                        st.warning('Error al ejecutar la query. Intente de nuevo modificando su consulta.')
 
             with col2:
                 try:
@@ -68,17 +91,18 @@ try:
                                 st.dataframe(local_vars['df_temp'])
 
                             except Exception as e:
-                                st.text('Error al ejecutar la query. Intente de nuevo modificando su consulta.')
+                                st.warning('Error al ejecutar la query. Intente de nuevo modificando su consulta.')
 
                     else:
                         try:
                             st.dataframe(local_vars['df_temp'])
 
                         except Exception as e:
-                            st.text(output)
+                            #st.text(output)
+                            st.warning('Error al ejecutar la query. Intente de nuevo modificando su consulta.')
 
                 except Exception as e:
-                    st.text('Error al ejecutar la query. Intente de nuevo modificando su consulta.')
+                    st.warning('Error al ejecutar la query. Intente de nuevo modificando su consulta.')
 
             st.session_state.user_input = ''
 
--- utils.py
+++ utils.py
@@ -12,18 +12,31 @@ from dotenv import load_dotenv
 from google.oauth2 import service_account
 import plotly.graph_objects as go
 import streamlit as st
+from langchain_google_genai import GoogleGenerativeAI, HarmBlockThreshold, HarmCategory
+from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
+
 
 # env
 load_dotenv()
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'key.json'
 PROJECT_ID = os.environ["PROJECT_ID"]
 LOCATION = os.environ["LOCATION"]
+GCP_API_KEY = os.environ["GCP_API_KEY"]
 vertexai.init(project=PROJECT_ID, location=LOCATION)
+GCP_MODEL_ID = "gemini-1.5-flash-002"
 
 
 def submit_query():
     st.session_state.user_input = st.session_state.widget
     st.session_state.widget = ''
+
+
+def reset_memory():
+    """resets assistant memory"""
+    st.session_state.ai_assistant = 0
+    st.session_state.chat_memory = ChatMessageHistory()
 
 
 def set_credentials(credential_path):
@@ -57,8 +70,6 @@ def parse_null_list(value):
         return value
 
 
-
-
 def execute_code(snippet, df: pd.DataFrame):
     # Strip the code snippet
     code = snippet.strip().strip('```python').strip('```').strip()
@@ -74,71 +85,77 @@ def execute_code(snippet, df: pd.DataFrame):
         return {}, f"Error: {e}"
 
 
-def analyze_table_gemini(query: str, df: pd.DataFrame, plot_type='plotly'):
-    generation_config = {
-        "max_output_tokens": 8192,  # max
-        "temperature": 1,
-        "top_p": 0.95,
-    }
+def generate_llm_chain(df: pd.DataFrame, model_name=GCP_MODEL_ID):
+    """generates a python snippet based on a user query using the GCP_MODEL_ID LLM model.
+    The query should be for solving something related to the df DataFrame"""
+    llm = GoogleGenerativeAI(model=model_name, google_api_key=GCP_API_KEY,
+                             generation_config={"max_output_tokens": 8192,  # max
+                                                "temperature": 1,  # deterministic output
+                                                "top_p": 0.95,
+                                                },
+                             safety_settings={
+                                 HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+                                 HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+                                 HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+                                 HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+                             })
 
-    safety_settings = {
-        generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    }
     if len(df) > 50:
         df_sample = df.sample(n=50, random_state=42)
     else:
         df_sample = df
 
-    df_parsed = df_sample.to_string(index=False) # random sample. Little hack
+    df_parsed = df_sample.to_string(index=False)  # random sample. Little hack for reducing Input tokens needed
+                                                  # Faster and cheaper.
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", [
+                "Eres un asistente de IA experto en análisis de datos y programación en el lenguaje python",
+                f"Debes responder preguntas relacionadas a la siguiente tabla de datos: {df_parsed}.",
+                "El usuario te proporcionará una consulta en lenguaje natural y debes responderla entregando un código en python.",
+                "Usa el nombre 'df' para la tabla que contenga la data. No vuelvas a generar la data en el código.",
+                f"Si vas a filtrar columnas de la tabla, siempre usa las siguientes columnas: {df.columns}."
+                "Si necesitas graficar, siempre usa la librería: 'plotly.express' sin imprimir la imagen (no uses .show())",
+                "Si necesitas graficar, siempre incluye un título para el gráfico",
+                "Piensa paso a paso, verificando que los formatos y tipos de datos sean los correctos y siempre importa las librerías necesarias en el código.",
+                "No imprimas comentarios en el código (no uses #).",
+                "En el caso en que tengas que entregar una tabla con la respuesta final a la consulta del usuario, llama a esta tabla: df_temp en el código generado.",
+                "No uses la función print para imprimir la tabla al final del código",
+                "Si no respondiste generando código en python, siempre respondes en español"],
+             ),
+            ("placeholder", "{chat_history}"),
+            ("human", "{input}"),
+        ])
 
-    if plot_type == 'plotly':
-        system_instruction = [
-            "Eres un asistente de IA experto en análisis de datos y programación en el lenguaje python",
-            f"Debes responder preguntas relacionadas a la siguiente tabla de datos: {df_parsed}.",
-            "El usuario te proporcionará una consulta en lenguaje natural y debes responderla entregando un código en python.",
-            "Usa el nombre 'df' para la tabla que contenga la data. No vuelvas a generar la data en el código.",
-            f"Si vas a filtrar columnas de la tabla, siempre usa las siguientes columnas: {df.columns}."
-            "Si necesitas graficar, siempre usa la librería: 'plotly' sin imprimir la imagen (no uses .show())",
-            "Si necesitas graficar, siempre incluye in título para el gráfico",
-            "Piensa paso a paso, verificando que los formatos y tipos de datos sean los correctos y siempre importa las librerías necesarias en el código.",
-            "No imprimas comentarios en el código (no uses #).",
-            "En el caso en que tengas que entregar una tabla con la respuesta final a la consulta del usuario, llama a esta tabla: df_temp en el código generado.",
-            "No uses la función print para imprimir la tabla al final del código",
-            "Si no respondiste generando código en python, siempre respondes en español",
-        ]
+    chain = prompt | llm
+    return chain
+
+
+def add_memory_chain(chain, chat_history):
+    """adds memory for adding more context for the queries """
+    #demo_ephemeral_chat_history_for_chain = ChatMessageHistory()
+
+    chain_with_message_history = RunnableWithMessageHistory(
+        chain,
+        lambda session_id: chat_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+    )
+    return chain_with_message_history
+
+
+def fix_query(query: str):
+    """fix results if user does not explicitly ask for a table. This fix should be monitored"""
+    check_words = ['gráfico', 'grafico', 'tabla', 'dataframe', 'plot', 'boxplot', 'histograma',
+                   'df', 'graficame', 'grafícame', 'plotea', 'ploteame', 'plotéame']
+
+    words_cap = list(map(lambda x: str(x).capitalize(), check_words))
+    words_upper = list(map(lambda x: str(x).upper(), check_words))
+    check_words.extend(words_cap)
+    check_words.extend(words_upper)
+    # check_words = words from list + words in capital letter + words with first string in Upper
+    if any(word in query for word in check_words):
+        return query
     else:
-        system_instruction = [
-            "Eres un asistente de IA experto en análisis de datos y programación en el lenguaje python.",
-            f"Debes responder preguntas relacionadas a la siguiente tabla de datos: {df_parsed}.",
-            "El usuario te proporcionará una consulta en lenguaje natural y debes responderla entregando un código en python.",
-            "Usa el nombre 'df' para la tabla que contenga la data. No vuelvas a generar la data en el código.",
-            f"Si vas a filtrar columnas de la tabla, siempre usa las siguientes columnas: {df.columns}."
-            "Piensa paso a paso, verificando que los formatos y tipos de datos sean los correctos y siempre importa las librerias necesarias en el codigo.",
-            "No imprimas comentarios en el código. (no uses #) y utiliza el comando print para imprimir la tabla con los datos de la consulta.",
-            "En el caso que tengas que imprimir una tabla con la respuesta final, llama a esta tabla: df_temp en el código generado."
-            "En el caso de que tengas que graficar, ocupa un fig_size fijo de (5,5) y siempre usa tight_layout.",
-            "Si no respondiste generando código en python, siempre respondes en español",
-            ]
-
-    model = GenerativeModel(
-        model_name="gemini-1.5-pro-001",
-        system_instruction=system_instruction)
-
-    prompt = f"""
-                 User input: {query}
-                 Answer:
-                """
-
-    contents = [prompt]
-
-    responses = model.generate_content(contents,
-                                       generation_config=generation_config,
-                                       safety_settings=safety_settings,
-                                       stream=False)  # Si True -> la parte de text tiene que ser iterada
-
-    # for response in responses:
-    #    print(response.text, end="")
-    return responses.text
+        fixed_query = query + ' en una tabla'
+        return fixed_query
